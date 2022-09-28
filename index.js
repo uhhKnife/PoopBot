f866e0156c413b79740a338885cdba78f5321d70
Lil Poop <108466592+uhhKnife@users.noreply.github.com>
2022-09-28 18:01:36 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const {Client, GatewayIntentBits, Routes, Collection} = require("discord.js");
 const botConfig = require("./botConfig.json");
-const fs = require("node;fs");
-const path = require('node:path')
-const { REST } = require("discord.js/rest");
+const fs = require("node:fs");
+const path = require('node:path');
+const { REST } = require('@discordjs/rest');
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 client.commands = new Collection();
@@ -10,10 +10,24 @@ const slashCommands = [];
 
 client.once("ready" , () => {
     console.log(`${client.user.username} is Online!`)
+    let guildId = botConfig.guildID;
+    let clientId = botConfig.clientID;
+    let token = botConfig.token;
+    const commands = [];
+    
+    const rest = new REST({ version: '10' }).setToken(token);
+
+    rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
+	.then(data => console.log(`Successfully registered ${data.length} application commands.`))
+	.catch(console.error);
+
+
+
 })
 
-const commandsPath = "./slashCommands";
+const commandsPath = path.join(__dirname, 'slashCommands');
 const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+const { clientId, guildId, token } = require('./botconfig.json');
 
 for (const file of commandFiles) {
 	const filePath = path.join(commandsPath, file);
@@ -23,12 +37,23 @@ for (const file of commandFiles) {
 	client.commands.set(command.data.name, command);
     slashCommands.push(command.data.toJSON());
     console.log(`${command.data.name} is loaded!`)
-    let guildId = botConfig.guildID;
-    let clientId = botConfig.clientID;
-    let token = botConfig.token;
 
 }
 
+client.on('interactionCreate', async interaction => {
+	if (!interaction.isChatInputCommand()) return;
+
+	const command = interaction.client.commands.get(interaction.commandName);
+
+	if (!command) return;
+
+	try {
+		await command.execute(client, interaction);
+	} catch (error) {
+		console.error(error);
+		await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+	}
+});
 
 
 
