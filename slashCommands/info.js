feff647c2abf0b45b43442c831b4891aa5723bd8
Lil Poop <108466592+uhhKnife@users.noreply.github.com>
2022-09-29 16:56:32 +0100

    fix

--- slashCommands/info.js
+++ slashCommands/info.js
@@ -1,23 +1,24 @@
 const { SlashCommandBuilder, EmbedBuilder} = require('discord.js');
 
 module.exports = {
-	category: "information",
+	category: 'information',
 	data: new SlashCommandBuilder()
 		.setName('info')
 		.setDescription('Gives info about the server!'),
 	async execute(client, interaction) {
             
         var botEmbed = new EmbedBuilder()
-            .setTitle("Title")
-            .setDiscription("Description")
-            .setColor("#ff80f7")
+            .setColor('#ff80f7')
+            .setTitle('Title')
+            .setDescription('Description')
+            
             .addFields(
-                {name: "Field 1", value: client.user.username }
+                {name: 'Field 1', value: client.user.username }
              )
-            .setThumbnail("https://i.postimg.cc/pVjTfWzN/tumbnail.gif")
-            .setimage("https://i.postimg.cc/kXBzLGMG/test.png")
-            .setFooter( {text: "Footer", iconURL: "https://i.postimg.cc/4dDxDqCF/iconfooter.png"}) 
-            .setAuthor ( {name: "Author", iconUR: "https://i.postimg.cc/2ynSrGrj/icontitle.png" } )
+            .setThumbnail('https://i.postimg.cc/pVjTfWzN/tumbnail.gif')
+            .setImage('https://i.postimg.cc/kXBzLGMG/test.png')
+            .setFooter( {text: 'Footer', iconURL: 'https://i.postimg.cc/4dDxDqCF/iconfooter.png'}) 
+            .setAuthor ( {name: 'Author', iconUR: 'https://i.postimg.cc/2ynSrGrj/icontitle.png' } )
 
         await interaction.reply({ embeds: [botEmbed] });
 	},
