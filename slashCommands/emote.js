92be0da61444113e25ed472e558e7ab3f5f581ab
Lil Poop <108466592+uhhKnife@users.noreply.github.com>
2022-10-01 16:47:32 +0100

    Added optional text parameter to emotes command

--- slashCommands/emote.js
+++ slashCommands/emote.js
@@ -1,7 +1,7 @@
 const { SlashCommandBuilder} = require('discord.js');
 
 module.exports = {
-    category: "fun"
+    category: "fun",
 	data: new SlashCommandBuilder()
 		.setName('emote')
 		.setDescription('Use normal/animated emotes!')
@@ -9,11 +9,26 @@ module.exports = {
             option.setName("emoji")
             .setDescription('Give a normal/animated emoji name from this server! Example: fight')
             .setRequired(true)
-            ),
+            )
+            .addStringOption(option =>
+                option.setName("text")
+                .setDescription('Give text you want behind your emoji, u cn leave this empty...')
+                .setRequired(false)
+                ),
 
 	async execute(client, interaction) {
+
+        let text = await interaction.options.getString("text");
+        if(!text){
+                const emote = await interaction.options.getString("emoji");
+                const emotename = client.emojis.cache.find(emoji => emoji.name ===  emote);
+            await interaction.reply(emotename.toString());
+        }
+        else{
+
         const emote = await interaction.options.getString("emoji");
         const emotename = client.emojis.cache.find(emoji => emoji.name ===  emote);
-    await interaction.reply(emotename.toString());
+    await interaction.reply(emotename.toString() + " " + text);
+}
 },
 };
